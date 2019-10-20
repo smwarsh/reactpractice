9dcb7509591f427b1d9050f7c475ffcd8be75d24
Steph Warsh <swarsh@greaterthanone.com>
2019-10-20 16:01:27 -0400

    Initial commit with items from Hello Div exercise

---
+++ src/components/HelloDiv.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HelloDiv() {
+    return (
+        <div>
+            <h1>Hello! My name is Steph</h1>
+            <p>These are 3 things I like:</p>
+            <ul>
+                <li>Gardening</li>
+                <li>Jiji</li>
+                <li>Peach</li>
+            </ul>
+        </div>
+    );
+};
+
+export default HelloDiv;
