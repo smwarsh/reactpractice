06fb6145d1a160f6210fdc765d6d19fbf5b91f45
Steph Warsh <swarsh@greaterthanone.com>
2019-10-21 16:33:12 -0400

    Replace src directory with new one for lesson 5 Hello Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import HelloDiv from "./components/HelloDiv";
 
-const App = () => <HelloDiv />;
+const App = () => <p>Render Bootstrap components here</p>;
 
 export default App;
