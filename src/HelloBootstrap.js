8db0f912c938a2ecc285c47c380c4cd269c13deb
Steph Warsh <swarsh@u.rochester.edu>
2019-10-21 17:43:04 -0400

    Add Navbar to the page! Learned that this is very different from the HTML syntax, which I tried initially

--- src/HelloBootstrap.js
+++ src/HelloBootstrap.js
@@ -1,8 +1,32 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Card from 'react-bootstrap/Card'; // Card replaces Bootstrap Panel in the newer Bootstrap 4. This course was written for Bootstrap 3.3
 
-const HelloBootstrap = () => <p>Write Bootstrap here.</p>;
+// const HelloBootstrap = () => <p>Write Bootstrap here.</p>;
+
+function HelloBootstrap() {
+    return(
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
 
 export default HelloBootstrap;
