d29e3f251a4702fe6a836821905fa9f46671c61f
Steph Warsh <swarsh@u.rochester.edu>
2019-10-22 14:12:26 -0400

    Create JavaScript variables and render them in JSX

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -1,12 +1,17 @@
 import React from "react";
 
+const name = 'Steph';
+const thoughtsOnReact = 'is powerful!';
+
+const numOfLetters = (word) => word.length;
+
 const JSXVariables = () => (
   <div className="main-container">
     <div className="container">
       <div className="jumbotron">
-        <h1>Hi! My name is (insert name here)</h1>
-        <h2>My name has (insert number of letters in name here) letters</h2>
-        <h2>I think React (insert thoughts about React here)</h2>
+        <h1>Hi! My name is {name}</h1>
+        <h2>My name has {numOfLetters(name)} letters</h2>
+        <h2>I think React {thoughtsOnReact}</h2>
       </div>
     </div>
   </div>
