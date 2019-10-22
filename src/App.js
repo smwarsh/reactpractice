0254484dc48ef4fe654eba9d38634df04a212ca9
Steph Warsh <swarsh@u.rochester.edu>
2019-10-22 13:58:20 -0400

    Replace files to start exercise 7: JSX variables

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import React from 'react';
-import MyNavbar from './components/MyNavbar.js';
-import MyJumbotron from './components/MyJumbotron.js';
-import MyCard from './components/MyCard.js';
+import React from "react";
+import JSXVariables from "./components/JSXVariables";
 
-const App = () => {
-    return(
-        <div>
-            <MyNavbar />
-            <MyJumbotron />
-            <MyCard />
-        </div>
-    );
-}
+const App = () => <JSXVariables />;
 
 export default App;
