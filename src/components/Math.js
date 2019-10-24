af6af23a535175a511530f791739ad0bd1f4caaf
Steph Warsh <swarsh@u.rochester.edu>
2019-10-24 15:21:20 -0400

    My solution for rendering the solution at a font size of that many pixels

--- src/components/Math.js
+++ src/components/Math.js
@@ -3,20 +3,25 @@ import React from "react";
 const Math = (props) => {
     const num1Int = Number(props.num1);
     const num2Int = Number(props.num2);
+    let answer;
+    
     switch (props.operator) {
         case '+':
-            return <span>{num1Int + num2Int}</span>;
+            answer = num1Int + num2Int;
             break;
         case '-':
-            return <span>{num1Int - num2Int}</span>;
+            answer = num1Int - num2Int;
             break;
         case '*':
-            return <span>{num1Int * num2Int}</span>;
+            answer = num1Int * num2Int;
             break;
         case '/':
-            return <span>{num1Int / num2Int}</span>;
+            answer = num1Int / num2Int;
             break;
     }
+
+    const fontStyle = {fontSize: answer};
+    return <span style={fontStyle}>{answer}</span>;
 };
 
 export default Math;
