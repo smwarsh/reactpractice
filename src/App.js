020d65553b0db2d6edc37d7e3dc65d058c271913
Steph Warsh <swarsh@u.rochester.edu>
2019-10-25 16:32:54 -0400

    Exchange src folder for unsolved lesson 12 - Props Review

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
-import Calculator from "./components/Calculator";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import SpongeBobCard from "./components/SpongeBobCard";
+import SquidwardCard from "./components/SquidwardCard";
+import MrKrabsCard from "./components/MrKrabsCard";
 
-const App = () => <Calculator />;
+const App = () => (
+  <Wrapper>
+    <Title>Friends List</Title>
+    <SpongeBobCard />
+    <MrKrabsCard />
+    <SquidwardCard />
+  </Wrapper>
+);
 
 export default App;
