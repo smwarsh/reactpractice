be8bf58f1eacaf6a4ffb660e1bf448b8aa169d48
Steph Warsh <swarsh@u.rochester.edu>
2019-10-25 17:36:10 -0400

    Cleaned up the app to only print the list of friends once + added a friend to the list and it gets added to the page perfectly

--- src/App.js
+++ src/App.js
@@ -4,22 +4,12 @@ import Title from "./components/Title";
 import friends from "./friends.json";
 import FriendCard from "./components/FriendCard";
 
-function printFriends(friendsArray) {
-  let printOut = [];
-  for (let i = 0; i < friendsArray.length; i++) {
-    printOut.push(<FriendCard name={friends[i].name} image={friends[i].image} occupation={friends[i].occupation} location={friends[i].location} />);
-  }
-  return printOut;
-}
-
-const printMyFriends = friends.map( i => <FriendCard name={i.name} image={i.image} occupation={i.occupation} location={i.location} /> );
+const printFriends = friends.map( i => <FriendCard name={i.name} image={i.image} occupation={i.occupation} location={i.location} /> );
 
 const App = () => (
   <Wrapper>
     <Title>Friends List</Title>
-    {printFriends(friends)}
-    {printMyFriends}
-    {friends.map( i => <FriendCard name={i.name} image={i.image} occupation={i.occupation} location={i.location} /> )}
+    {printFriends}
   </Wrapper>
 );
 
