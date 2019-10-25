250a21194fb3ab55f7f7367692b3bc007fffe7c2
Steph Warsh <swarsh@u.rochester.edu>
2019-10-25 17:09:11 -0400

    Make program more DRY by passing values to FriendCard, which uses the props dynamically to output content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
+import friends from "./friends.json";
 import FriendCard from "./components/FriendCard";
-import SpongeBobCard from "./components/SpongeBobCard";
-import SquidwardCard from "./components/SquidwardCard";
-import MrKrabsCard from "./components/MrKrabsCard";
 
 const App = () => (
   <Wrapper>
     <Title>Friends List</Title>
-    <FriendCard />
+    <FriendCard name={friends[0].name} image={friends[0].image} occupation={friends[0].occupation} location={friends[0].location} />
+    <FriendCard name={friends[1].name} image={friends[1].image} occupation={friends[1].occupation} location={friends[1].location} />
+    <FriendCard name={friends[2].name} image={friends[2].image} occupation={friends[2].occupation} location={friends[2].location} />
   </Wrapper>
 );
 
--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -1,28 +1,31 @@
 import React from "react";
 import "./FriendCard.css";
 
-const FriendCard = () => (
-  <div className="card">
-    <div className="img-container">
-      <img
-        alt="SpongeBob"
-        src="https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Spongebob-squarepants.svg/666px-Spongebob-squarepants.svg.png"
-      />
-    </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> SpongeBob
-        </li>
-        <li>
-          <strong>Occupation:</strong> Fry Cook
-        </li>
-        <li>
-          <strong>Location:</strong> A Pinapple Under the Sea
-        </li>
-      </ul>
-    </div>
-  </div>
-);
+const FriendCard = props => {
+    
+    return(
+        <div className="card">
+            <div className="img-container">
+            <img
+                alt={props.name}
+                src={props.image}
+            />
+            </div>
+            <div className="content">
+            <ul>
+                <li>
+                <strong>Name:</strong> {props.name}
+                </li>
+                <li>
+                <strong>Occupation:</strong> {props.occupation}
+                </li>
+                <li>
+                <strong>Location:</strong> {props.location}
+                </li>
+            </ul>
+            </div>
+        </div>
+    )
+};
 
 export default FriendCard;
