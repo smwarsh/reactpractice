65fb24b70515b52f233cb3304807ba99a3c3b81f
Steph Warsh <swarsh@u.rochester.edu>
2019-10-28 14:50:37 -0400

    Use filter function to only print grocery list items that have not been purchased

--- src/components/List.js
+++ src/components/List.js
@@ -3,7 +3,7 @@ import React from "react";
 const List = props => (
   <ul className="list-group">
     {/* From React docs: A good rule of thumb is that elements inside the map() call need keys. */}
-    {props.groceries.map(item => <li key={item.id}>{item.name}</li>)}
+    {props.groceries.filter(item => !item.purchased).map(item => <li key={item.id}>{item.name}</li>)}
   </ul>
 );
 
