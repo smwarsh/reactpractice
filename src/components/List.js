e0661f811f35a7b3a69cbc5235bd1b1b09d66b6a
Steph Warsh <swarsh@u.rochester.edu>
2019-10-28 11:31:26 -0400

    Use map function to print a list of groceries with keys

--- src/components/List.js
+++ src/components/List.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const List = props => (
   <ul className="list-group">
-    Using the map method, render one li element displaying the text property of
-    each grocery object.
+    {/* From React docs: A good rule of thumb is that elements inside the map() call need keys. */}
+    {props.groceries.map(item => <li key={item.id}>{item.name}</li>)}
   </ul>
 );
 
