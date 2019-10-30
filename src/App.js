b49d3347725dad18d2e14b75f26c4954184a1ea0
Steph Warsh <swarsh@u.rochester.edu>
2019-10-30 15:38:27 -0400

    Exchange src folder for instructor lesson 17 - Forms Demo (for reference)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,6 @@
 import React from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import friends from "./friends.json";
-import "./App.css";
+import Form from "./components/Form";
 
-class App extends React.Component {
-  state = {
-    friends: friends
-  };
-
-  removeFriend = (id) => {
-    this.setState({friends: this.state.friends.filter(friend => friend.id !== id)});
-  }
-
-  render() {
-    return(
-      <Wrapper>
-        <h1 className="title">Friends List</h1>
-        {this.state.friends.map( friend =>
-          <FriendCard
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-            id={friend.id}
-            remove={this.removeFriend}
-          />
-        )}
-      </Wrapper>
-    );
-  }
-}
+const App = () => <Form />;
 
 export default App;
