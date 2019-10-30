15c661602309cf479bf14725914c55d145565177
Steph Warsh <swarsh@u.rochester.edu>
2019-10-30 14:25:14 -0400

    Map through each object in friends JSON file to print all to the page

--- src/App.js
+++ src/App.js
@@ -7,24 +7,14 @@ import "./App.css";
 const App = () => (
   <Wrapper>
     <h1 className="title">Friends List</h1>
-    <FriendCard
-      name={friends[0].name}
-      image={friends[0].image}
-      occupation={friends[0].occupation}
-      location={friends[0].location}
-    />
-    <FriendCard
-      name={friends[1].name}
-      image={friends[1].image}
-      occupation={friends[1].occupation}
-      location={friends[1].location}
-    />
-    <FriendCard
-      name={friends[2].name}
-      image={friends[2].image}
-      occupation={friends[2].occupation}
-      location={friends[2].location}
-    />
+    {friends.map( friend =>
+      <FriendCard
+        name={friend.name}
+        image={friend.image}
+        occupation={friend.occupation}
+        location={friend.location}
+      />
+    )}
   </Wrapper>
 );
 
