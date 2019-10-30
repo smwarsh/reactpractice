0d343159e68196020a3578eee4eda2dbe836768d
Steph Warsh <swarsh@u.rochester.edu>
2019-10-30 14:39:47 -0400

    Refactor the  component so that it's a class component. Set the component's initial state to the  JSON array. Inside of the  method, map over  to render each  component.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import Wrapper from "./components/Wrapper";
 import friends from "./friends.json";
 import "./App.css";
 
-const App = () => (
-  <Wrapper>
-    <h1 className="title">Friends List</h1>
-    {friends.map( friend =>
-      <FriendCard
-        key={friend.id}
-        name={friend.name}
-        image={friend.image}
-        occupation={friend.occupation}
-        location={friend.location}
-      />
-    )}
-  </Wrapper>
-);
+class App extends React.Component {
+  state = {
+    friends: friends
+  };
+
+  render() {
+    return(
+      <Wrapper>
+        <h1 className="title">Friends List</h1>
+        {this.state.friends.map( friend =>
+          <FriendCard
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        )}
+      </Wrapper>
+    );
+  }
+}
 
 export default App;
